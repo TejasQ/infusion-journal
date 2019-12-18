86dabd288039029d3c072ece0f6d762f8d796917
Tejas Kumar <tejas@contiamo.com>
2019-12-18 12:09:47 +0100

    Deploy to Now

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const webpack = require("webpack");
+const { join } = require("path");
 
 module.exports = {
+  output: {
+    path: join(__dirname, "public"),
+  },
   plugins: [new webpack.EnvironmentPlugin(["SCHEMA_URL"])],
 };
